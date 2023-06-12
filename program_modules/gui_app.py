ec2d7be1c7b2aa6083c95b27404e108923492baa
Thomas <thomasbourgeois2@hotmail.com>
2023-06-11 21:21:39 -0700

    formatted code

--- program_modules/gui_app.py
+++ program_modules/gui_app.py
@@ -4,9 +4,9 @@ Script to run PyQt app functions and classes.
 
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5 import QtWidgets
-from PyQt5 import uic
 from PyQt5.QtCore import QUrl
 from PyQt5.QtGui import QDesktopServices
+from PyQt5 import uic
 import pandas as pd
 import sys
 import os
@@ -155,7 +155,7 @@ class MainWindow(QMainWindow):
         elif website.lower() == 'adafruit':
             link = 'https://www.adafruit.com/'
 
-        elif website.lower() ==  'bc robotics':
+        elif website.lower() == 'bc robotics':
             link = 'https://bc-robotics.com/'
 
         QDesktopServices.openUrl(QUrl(link))
