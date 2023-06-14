725aaca3ea3518573ecd3dc63ce4d452097e5fba
Thomas <thomasbourgeois2@hotmail.com>
2023-06-13 23:12:09 -0700

    added script for project window

---
+++ Program_Files/project_window.py
@@ -0,0 +1,21 @@
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5 import QtWidgets
+from PyQt5 import uic
+import sys
+
+
+class Info_Window(QMainWindow):
+    def __init__(self, parent=None):
+        super(Info_Window, self).__init__(parent)
+
+        # loading ui file
+        uic.loadUi('Program_Files/project_window.ui', self)
+        
+
+
+if __name__ == "__main__":
+    # runnning program
+    app = QApplication(sys.argv)
+    info_window = Info_Window()
+    info_window.show()
+    app.exec_()
