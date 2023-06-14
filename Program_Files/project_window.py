f2b3da865c8f51fb3da4c3c33ca26fceb99e91b9
Thomas <thomasbourgeois2@hotmail.com>
2023-06-13 23:16:38 -0700

    change class name

--- Program_Files/project_window.py
+++ Program_Files/project_window.py
@@ -10,9 +10,9 @@ from PyQt5 import uic
 import sys
 
 
-class Info_Window(QMainWindow):
+class Project_Window(QMainWindow):
     def __init__(self, parent=None):
-        super(Info_Window, self).__init__(parent)
+        super(Project_Window, self).__init__(parent)
 
         # loading ui file
         uic.loadUi('Program_Files/project_window.ui', self)
@@ -22,6 +22,6 @@ class Info_Window(QMainWindow):
 if __name__ == "__main__":
     # runnning program
     app = QApplication(sys.argv)
-    info_window = Info_Window()
-    info_window.show()
+    Project_Window = Project_Window()
+    Project_Window.show()
     app.exec_()
