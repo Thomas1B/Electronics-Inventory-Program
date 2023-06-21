a04df1ff919200773ea3d79ec05faf29b2d82bb2
Thomas <thomasbourgeois2@hotmail.com>
2023-06-20 23:31:19 -0700

    added comments

--- Program_Files/info_windows.py
+++ Program_Files/info_windows.py
@@ -39,15 +39,17 @@ class Add_Item_Window(QMainWindow):
         super(Add_Item_Window, self).__init__()
         uic.loadUi('Program_Files/UI_Files/adding_item_window.ui', self)
 
+        # frames for plainTextEdit Boxes
         self.frame2 = self.findChild(QtWidgets.QFrame, 'frame_2')
         self.frame3 = self.findChild(QtWidgets.QFrame, 'frame_3')
         self.frame4 = self.findChild(QtWidgets.QFrame, 'frame_4')
         self.frame5 = self.findChild(QtWidgets.QFrame, 'frame_5')
         self.frame6 = self.findChild(QtWidgets.QFrame, 'frame_6')
         self.frame7 = self.findChild(QtWidgets.QFrame, 'frame_7')
+
+        # adding stylesheet to sub frames 2-7
         frames = [self.frame2, self.frame3, self.frame4,
                   self.frame5, self.frame6, self.frame7]
-
         for frame in frames:
             frame.setStyleSheet(
                 '''
@@ -61,4 +63,3 @@ class Add_Item_Window(QMainWindow):
             )
 
         self.show()
-
