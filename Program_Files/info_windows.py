b8165cd0a5501dfc888a44a6bf838668e9a30cda
Thomas <thomasbourgeois2@hotmail.com>
2023-06-20 22:05:18 -0700

    updated new window

--- Program_Files/info_windows.py
+++ Program_Files/info_windows.py
@@ -33,17 +33,32 @@ class User_Info_Window(QMainWindow):
 
         self.show()
 
+
 class Add_Item_Window(QMainWindow):
     def __init__(self):
         super(Add_Item_Window, self).__init__()
         uic.loadUi('Program_Files/UI_Files/adding_item_window.ui', self)
-        
+
+        self.frame2 = self.findChild(QtWidgets.QFrame, 'frame_2')
+        self.frame3 = self.findChild(QtWidgets.QFrame, 'frame_3')
+        self.frame4 = self.findChild(QtWidgets.QFrame, 'frame_4')
+        self.frame5 = self.findChild(QtWidgets.QFrame, 'frame_5')
+        self.frame6 = self.findChild(QtWidgets.QFrame, 'frame_6')
+        self.frame7 = self.findChild(QtWidgets.QFrame, 'frame_7')
+        frames = [self.frame2, self.frame3, self.frame4,
+                  self.frame5, self.frame6, self.frame7]
+
+        for frame in frames:
+            frame.setStyleSheet(
+                '''
+                QFrame {
+                    border: 2px solid black;
+                }
+                QLabel {
+                    border: none;
+                }
+                '''
+            )
+
         self.show()
 
-
-if __name__ == "__main__":
-    # runnning program
-    app = QApplication(sys.argv)
-    info_window = Info_Window()
-    info_window.show()
-    app.exec_()
